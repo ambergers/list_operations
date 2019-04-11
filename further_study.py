d39bca5d28c755dd3875501624423d6b34094d5a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-11 12:41:16 -0700

    added logic for custom_len function

--- further_study.py
+++ further_study.py
@@ -30,8 +30,10 @@ def custom_len(input_list):
         8
 
     """
-
-    return 0
+    cnt = 0
+    for item in input_list:
+        cnt += 1
+    return cnt
 
 
 # For the next four exercises, you'll need to be clever and think about ways
